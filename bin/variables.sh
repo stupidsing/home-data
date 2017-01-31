6b1d92daf8fe4fd7f5635a39e9e0772ccd8dd800
stupidsing <stupidsing@gmail.com>
2017-02-01 04:15:05 +0800

    hi-dpi scaling

--- bin/variables.sh
+++ bin/variables.sh
@@ -1,5 +1,5 @@
-#export GDK_SCALE=2
-export GDK_DPI_SCALE=2
+export GDK_SCALE=2
+export GDK_DPI_SCALE=0.5
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386
 #export JAVA_HOME=$(find /opt/ -maxdepth 1 -name jdk\* | sort | tail -1)
 export M2_HOME=$(find /opt/ -maxdepth 1 -name apache-maven-\* | sort | tail -1)

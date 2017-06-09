83c1de1ff7cbf8ffbbe7c8847e2c979ee445e92e
stupidsing <stupidsing@gmail.com>
2017-06-09 19:57:11 +0800

    -

--- bin/set-home.sh
+++ bin/set-home.sh
@@ -21,17 +21,36 @@ runIfNotThere () {
 
 cd ${HOME}
 
-runIfNotThere /opt/google/chrome/chrome --incognito
-#runIfNotThere /opt/idea-IC-129.451/bin/idea.sh
-runIfNotThere icedove
-runIfNotThere iceweasel
-#runIfNotThere minitube
+# BEGIN set sound
+N=$(cat /proc/asound/cards | grep V19 | cut -d' ' -f2)
+echo "pcm.!default {
+	type hw
+	card ${N}
+}
+ctl.!default {
+	type hw
+	card ${N}
+}
+"> ~/.asoundrc
+# END set sound
+
+runIfNotThere chromium --incognito http://www.youtube.com/
+runIfNotThere evince
+runIfNotThere firefox
 runIfNotThere pidgin
+runIfNotThere scite /tmp/orders pad ${HOME}/workspace/home-data/stock.txt
+runIfNotThere thunderbird
 runIfNotThere tint2
 runIfNotThere terminator -m
+runIfNotThere xfce4-clipman
 runIfNotThere yeahconsole
 (export GTK2_RC_FILES=/home/ywsing/.tight-gtkrc-2.0 && export SWT_GTK3=0 && runIfNotThere /opt/eclipse/eclipse -data /home/ywsing/workspace)
 
+#runIfNotThere /opt/idea-IC-129.451/bin/idea.sh
+#runIfNotThere minitube
+#(export PYTHONPATH=/home/ywsing/terminator/install/lib/python2.7/site-packages:/usr/share/terminator && runIfNotThere /home/ywsing/terminator/install/bin/terminator -m)
 #runIfNotThere tilda
+#runIfNotThere wifi.sh
+#runIfNotThere xcompmgr -cfF -t-9 -l-11 -r9 -o.95 -D6
 
 sleep 2

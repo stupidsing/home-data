ce8791241350dbb5c24b1f288d2098a15ad1e2e8
stupidsing <stupidsing@gmail.com>
2017-09-28 06:40:45 +0800

    -

--- bin/variables.sh
+++ bin/variables.sh
@@ -1,6 +1,6 @@
 export GDK_SCALE=2
 export GDK_DPI_SCALE=0.5
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-i386
+export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-i386
 #export JAVA_HOME=$(find /opt/ -maxdepth 1 -name jdk\* | sort | tail -1)
 export M2_HOME=$(find /opt/ -maxdepth 1 -name apache-maven-\* | sort | tail -1)
 export PATH=/opt/go_appengine:${JAVA_HOME}/bin:${M2_HOME}/bin:${HOME}/bin:${PATH}

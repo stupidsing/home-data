ddb6f57ac2061a5934560cc2bb474d608dab53c7
ywsing <stupidsing@gmail.com>
2020-07-28 17:17:28 +0800

    -

--- bin/set-home.sh
+++ bin/set-home.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 PATH=/bin:/usr/bin:${PATH}
 
@@ -11,9 +11,6 @@ runIfNotThere() {
 		echo "To run $1..."
 
 		(
-#			if [ "$1" = "firefox" ]; then
-#				ulimit -v 1536000
-#			fi
 			nohup $* > /dev/null
 		) &
 	fi
@@ -21,35 +18,23 @@ runIfNotThere() {
 
 cd ~
 
-# BEGIN set sound
-N=$(cat /proc/asound/cards | grep V19 | cut -d' ' -f2)
-echo "pcm.!default {
-	type hw
-	card ${N}
-}
-ctl.!default {
-	type hw
-	card ${N}
-}
-"> ~/.asoundrc
-# END set sound
-
 runIfNotThere chromium --force-device-scale-factor=2 --high-dpi-support=1 --incognito http://finance.yahoo.com/ https://www.youtube.com/ file:///data/storey file:///home/ywsing/docs
 runIfNotThere evince
 runIfNotThere firefox
+runIfNotThere lxpanel
 runIfNotThere pcmanfm ~/docs/
 runIfNotThere scite pad ~/home-data/stock.txt ~/bin/variables.sh
 runIfNotThere thunderbird
-runIfNotThere tint2
-runIfNotThere terminator -m
+runIfNotThere /usr/bin/x-terminal-emulator -m
 runIfNotThere xfce4-clipman
 runIfNotThere yeahconsole
-(GTK2_RC_FILES=~/.tight-gtkrc-2.0 GTK_IM_MODULE= SWT_GTK3=0 runIfNotThere ${ECLIPSE_HOME}/eclipse -data ~/workspace)
+runIfNotThere ${ECLIPSE_HOME}/eclipse -data ~/workspace
 
 #runIfNotThere /opt/idea-IC-129.451/bin/idea.sh
 #runIfNotThere minitube
 #(export PYTHONPATH=~/terminator/install/lib/python2.7/site-packages:/usr/share/terminator && runIfNotThere ~/terminator/install/bin/terminator -m)
 #runIfNotThere tilda
+#runIfNotThere tint2
 #runIfNotThere wifi.sh
 #runIfNotThere xcompmgr -cfF -t-9 -l-11 -r9 -o.95 -D6
 #(GTK2_RC_FILES=~/.tight-gtkrc-2.0 GTK_IM_MODULE= SWT_GTK3=0 runIfNotThere ${ECLIPSE_CPP_HOME}/eclipse -data ~/workspace.cpp)
